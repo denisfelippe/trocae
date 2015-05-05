dc0c86b79d4b3d32be0baa4b2ca25a6a200015f7
Denis Felippe da Rocha <denisfelippe@gmail.com>
2015-05-05 20:09:55 -0300

    Initial Commit

---
+++ trocae/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trocae
+//
+//  Created by Denis Felippe da Rocha on 05/05/15.
+//  Copyright (c) 2015 Intrare Web. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
